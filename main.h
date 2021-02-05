b9b73cc1b68916d3338ac5643aeb2d9973e8e948
Aleksander Shulzhik <gtorling@gmail.com>
2021-02-05 17:19:06 +0300

    Lesson3

---
+++ main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+/*3*. Написать программу, вычисляющую выражение из первого задания, а переменные для неё инициализировать в другом файле*/
+
+extern const int a = 2;
+extern const int b = 3;
+extern const int c = 4;
+extern const int d = 5;
